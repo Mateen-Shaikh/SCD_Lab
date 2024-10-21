3997da8e5a6d0b39942eec82465b60f806bafe12
Abdul Mateen <125717446+Mateen-Shaikh@users.noreply.github.com>
2024-10-21 15:51:29 +0500

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,12 @@
+
+public class Lab1 {
+	public static void main(String[] args) {
+		
+	double radius = 25.01;
+	
+	double result = 3.1415 * radius * radius;
+	
+	System.out.println("The Area of Circle with radius "+radius+" is "+result);
+		
+	}
+}
